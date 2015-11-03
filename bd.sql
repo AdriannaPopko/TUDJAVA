1019e1b5046770172dbedad66cf042bea888b2ca
AdriannaPopko <adunia_93@op.pl>
2015-11-03 17:17:25 +0100

    baza3

--- bd.sql
+++ bd.sql
@@ -13,9 +13,7 @@ CREATE TABLE opiekun (
   opiekun_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   opiekun_imie varchar(255) NOT NULL,
   opiekun_nazwisko varchar(255) NOT NULL,
-  opiekun_nr_tel int(9) NOT NULL,
-  adres_id int(11) NOT NULL,
-  FOREIGN KEY (adres_id) REFERENCES adres(adres_id)
+  opiekun_nr_tel int(9) NOT NULL
 )
 
 -- --------------------------------------------------------
@@ -28,9 +26,7 @@ CREATE TABLE wlasciciel (
   wlasciciel_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   wlasciciel_imie varchar(255) NOT NULL,
   wlasciciel_nazwisko varchar(255) NOT NULL,
-  wlasciciel_nr_tel int(9) NOT NULL,
-  adres_id int(11) NOT NULL,
-  FOREIGN KEY (adres_id) REFERENCES adres(adres_id)
+  wlasciciel_nr_tel int(9) NOT NULL
 )
 
 -- --------------------------------------------------------
@@ -43,41 +39,11 @@ CREATE TABLE zwierze (
   zwierze_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   zwierze_imie varchar(255) NOT NULL,
   zwierze_gatunek varchar(255) NOT NULL,
-  ulubione_jedzenie_id int(11) NOT NULL,
   opiekun_id int(11) NOT NULL,
   wlasciciel_id int(11) NOT NULL,
   zwierze_data_przyjecia date NOT NULL,
-  FOREIGN KEY (opiekun_id) REFERENCES customers(opiekun_id),
-  FOREIGN KEY (wlasciciel_id) REFERENCES customers(wlasciciel_id),
-  FOREIGN KEY (ulubione_jedzenie_id) REFERENCES customers(ulubione_jedzenie_id)
+  FOREIGN KEY (opiekun_id) REFERENCES opiekun(opiekun_id),
+  FOREIGN KEY (wlasciciel_id) REFERENCES wlasciciel(wlasciciel_id)
 )
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ulubione_jedzenie`
---
-
-CREATE TABLE ulubione_jedzenie (
-  ulubione_jedzenie_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  ulubione_jedzenie_nazwa varchar(255) NOT NULL,
-  ulubione_jedzenie_producent varchar(255) NOT NULL
-)
-
--- --------------------------------------------------------
-
-
---
--- Table structure for table `adres`
---
-
-CREATE TABLE adres (
-  adres_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  adres_miasto varchar(50) NOT NULL,
-  adres_kod varchar(50) NOT NULL,
-  adres_ulica varchar(225) NOT NULL,
-  adres_nr_domu int(11) NOT NULL,
-  adres_nr_mieszkania int(11) NOT NULL
-) 
 
 -- --------------------------------------------------------
