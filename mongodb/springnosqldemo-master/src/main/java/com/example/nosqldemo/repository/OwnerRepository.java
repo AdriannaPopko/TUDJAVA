23c3658594b567d970c87ac56b60b090957e5add
AdriannaPopko <adunia_93@op.pl>
2016-02-03 20:32:32 +0100

    owner repository fixed, owner test done

--- mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/repository/OwnerRepository.java
+++ mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/repository/OwnerRepository.java
@@ -3,6 +3,7 @@ package com.example.nosqldemo.repository;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.nosqldemo.domain.Owner;
@@ -14,5 +15,11 @@ public interface OwnerRepository extends CrudRepository<Owner, ObjectId> {
 	List<Owner> findByLname(String lname);
 	
 	Owner findById(ObjectId id);
+
+    List<Owner> findAll();
+
+    @Query(value = "{ lname : {$regex: ?0 } }")
+    List<Owner> findByLnamee(String lname);
+
 
 }
--- mongodb/springnosqldemo-master/src/test/java/com/example/nosqldemo/repository/OwnerManagerTest.java
+++ mongodb/springnosqldemo-master/src/test/java/com/example/nosqldemo/repository/OwnerManagerTest.java
@@ -1,59 +1,160 @@
-package com.example.nosqldemo.repository;
+ package com.example.nosqldemo.repository;
 
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bson.types.ObjectId;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.example.nosqldemo.domain.Car;
-import com.example.nosqldemo.domain.Person;
+import com.example.nosqldemo.domain.Pet;
+import com.example.nosqldemo.domain.Owner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/beans.xml" })
-public class PersonManagerTest {
-	
-	@Autowired
-	PersonRepository personManager;
-	@Autowired
-	CarRepository carRepository;
-	
-	@Test
-	public void checkAdding(){
-		
-		Person p1 = new Person();
-		p1.setName("Gosia");
-		
-		Car c1 = new Car();
-		c1.setMake("Toyota");
-		c1.setModel("Yaris");
-		carRepository.save(c1);
-		
-		
-		Car c2 = new Car();
-		c2.setMake("Honda");
-		c2.setModel("Accord");
-		carRepository.save(c2);
-		
-		List<Car> cars = new ArrayList<Car>();
-		cars.add(c1);
-		cars.add(c2);
-		
-		p1.setCars(cars);
-		
-		
-		personManager.save(p1);
-		
-		Person aGirl = personManager.findById(p1.getId());
-		
-		assertEquals(2, aGirl.getCars().size());
-		
-		
-	}
+public class OwnerManagerTest {
 
+    @Autowired
+    OwnerRepository ownerManager;
+
+    private final String fname1 = "fname1";
+    private final String lname1 = "lname1";
+
+    private final String fname2 = "fname2";
+    private final String lname2 = "lname2";
+
+    private List<ObjectId> allOwners = new ArrayList<ObjectId>();
+
+    @Before
+    public void controlRequest() {
+
+        Iterable<Owner> owners = ownerManager.findAll();
+
+        for (Owner o : owners) {
+            allOwners.add(o.getId());
+        }
+    }
+
+    @After
+    public void restoringDB() {
+        Iterable<Owner> owners = ownerManager.findAll();
+
+        boolean check;
+
+        for (Owner o : owners) {
+            check = true;
+            if (allOwners.contains(o.getId())) {
+                check = false;
+            }
+            if (check) {
+            	ownerManager.delete(o);
+            }
+        }
+    }
+    @Test
+    public void checkAdding(){
+    	Owner owner = new Owner();
+    	owner.setFname(fname1);
+    	owner.setLname(lname1);
+        ownerManager.save(owner);
+
+        Owner o = ownerManager.findById(owner.getId());
+
+        assertFalse(allOwners.contains(o.getId()));
+        assertEquals(o.getFname(),fname1);
+        assertEquals(o.getLname(),lname1);
+    }
+
+    @Test
+    public void checkUpdate(){
+    	Owner owner = new Owner();
+    	owner.setFname(fname1);
+    	owner.setLname(lname1);
+    	ownerManager.save(owner);
+
+        Owner o = ownerManager.findById(owner.getId());
+
+        assertEquals(fname1, o.getFname());
+        assertEquals(lname1, o.getLname());
+
+        o.setFname(fname2);
+        o.setLname(lname2);
+        ownerManager.save(o);
+
+        o = ownerManager.findById(owner.getId());
+
+        assertEquals(fname2, o.getFname());
+        assertEquals(lname2, o.getLname());
+
+        List<Owner> list = ownerManager.findAll();
+        assertEquals(list.size(),allOwners.size()+1);
+
+    }
+
+    @Test
+    public void checkFindByLname(){
+        List<Owner> ownerList = ownerManager.findByLname("lastn");
+        Owner owner = new Owner();
+        owner.setFname(fname1);
+        owner.setLname(lname1);
+        ownerManager.save(owner);
+
+        Owner owner2 = new Owner();
+        owner2.setFname(fname2);
+        owner2.setLname(lname2);
+        ownerManager.save(owner2);
+
+        List<Owner> ownerList2 = ownerManager.findByLname("lastn");
+        assertEquals(ownerList2.size(),ownerList.size()+2);
+    }
+
+    @Test
+    public void checkDelete(){
+    	Owner owner = new Owner();
+    	owner.setFname(fname1);
+    	owner.setLname(lname1);
+    	ownerManager.save(owner);
+        ObjectId id = owner.getId();
+        ownerManager.delete(owner);
+        List<Owner> list = ownerManager.findAll();
+        Owner o = ownerManager.findById(id);
+
+        assertEquals(allOwners.size(),list.size());
+        assertNull(o);
+    }
+
+    @Test
+    public void findById() {
+    	Owner owner = new Owner();
+    	owner.setFname(fname1);
+    	owner.setLname(lname1);
+    	ownerManager.save(owner);
+
+        Owner o = ownerManager.findById(owner.getId());
+
+        assertEquals(o.getFname(), owner.getFname());
+        assertEquals(o.getLname(), owner.getLname());
+    }
+
+    @Test
+    public void findByFname(){
+        List<Owner> ownerList = ownerManager.findByFname(fname1);
+
+        Owner owner = new Owner();
+        owner.setFname(fname1);
+        owner.setLname(lname1);
+        ownerManager.save(owner);
+
+        List<Owner> ownerList1 = ownerManager.findByFname(fname1);
+
+        assertEquals(ownerList1.size(),ownerList.size()+1);
+    }
 }
+
