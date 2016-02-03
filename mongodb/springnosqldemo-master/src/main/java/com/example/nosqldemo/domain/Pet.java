965ff496411c8b5e87bd924e450e061eedf40dd2
AdriannaPopko <adunia_93@op.pl>
2016-02-03 18:10:25 +0100

    pet fixed, manager fixed

--- mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/domain/Pet.java
+++ mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/domain/Pet.java
@@ -1,5 +1,7 @@
 package com.example.nosqldemo.domain;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 
 public class Pet {
@@ -7,6 +9,8 @@ public class Pet {
 	private ObjectId id;
 	private String name;
 	private String species;
+	private List<Owner> owners;
+
 
 	public ObjectId getId() {
 		return id;
@@ -31,5 +35,13 @@ public class Pet {
 	public void setSpecies(String species) {
 		this.species = species;
 	}
+
+    public List<Owner> getOwners(){
+        return this.owners;
+    }
+
+    public void setPers(List<Owner> owners) {
+        this.owners = owners;
+    }
 
 }
--- mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/service/PetManager.java
+++ mongodb/springnosqldemo-master/src/main/java/com/example/nosqldemo/service/PetManager.java
@@ -6,31 +6,43 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.nosqldemo.domain.Owner;
 import com.example.nosqldemo.domain.Pet;
+import com.example.nosqldemo.repository.OwnerRepository;
 import com.example.nosqldemo.repository.PetRepository;
 
 @Component
 public class PetManager {
 
-	@Autowired
-	private  PetRepository petRepository;
-	
-	public void addNewPet(Pet pet){
-		petRepository.save(pet);
-	}
-	
-	public List<Pet> getPets(String name){
-		return petRepository.findByName(name);
-	}
-	
-	public List<Pet> getPets(String name, String species){
-		return petRepository.znajdzZwierze(name, species);
-	}
-	
-	public Pet getPet(ObjectId id){
-		return petRepository.findById(id);
-	}
-	
-	
-	
+    @Autowired
+    private  PetRepository petRepository;
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+
+    public void addPet(Pet pet){ petRepository.save(pet); }
+    public void deletePet(Pet pet){ petRepository.delete(pet);}
+    public void updatePet(Pet pet, String species) {
+    	pet.setSpecies(species);
+    }
+    public List<Owner> getPersInRoom(Pet pet) { return pet.getOwners();}
+    public void deletePerInRoom(Pet pet,Owner owner){
+    	pet.getOwners().remove(owner);
+    	petRepository.save(pet);
+        ownerRepository.delete(owner);
+    }
+    public Iterable<Pet> findAllPet() { return petRepository.findAll(); }
+    public void deleteOwnersInPetByLname(Pet pet, String lname){
+        List<Owner> ownerList = pet.getOwners();
+        for(int i = 0; i < ownerList.size();i++){
+            if(ownerList.get(i).getLname() == lname) {
+                ObjectId id = ownerList.get(i).getId();
+                ownerList.remove(i);
+                ownerRepository.delete(id);
+                i--;
+            }
+        }
+        petRepository.save(pet);
+
+    }
 }
